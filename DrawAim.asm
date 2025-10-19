eeecfba9510f83fdce990ecdddc9403ef37cc79e
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-19 13:00:06 -0400

    push2

--- DrawAim.asm
+++ DrawAim.asm
@@ -1,6 +1,6 @@
 DrawAim PROC USES eax ebx ecx edx esi edi,
     x0:DWORD, y0:DWORD, deg:SDWORD
-    ; clamp 0..180, then index 15-degree steps
+    ; 0..180, then index 15-degree steps
     mov eax, deg
     cmp eax, 0
     jge @ok1
@@ -11,3 +11,15 @@ DrawAim PROC USES eax ebx ecx edx esi edi,
 @ok2: mov ebx, 15
     cdq
     idiv ebx
+mov esi, 1   ; dx default
+    mov edi, 0   ; dy default
+    cmp eax, 0
+    je  DrawAim_Lset
+    cmp eax, 1
+    je  DrawAim_L1
+    cmp eax, 2
+    je  DrawAim_L2
+    cmp eax, 3
+    je  DrawAim_L3
+    cmp eax, 4
+    je  DrawAim_L4
