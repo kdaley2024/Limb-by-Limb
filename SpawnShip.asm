c2be78958a4090a5a863f81595f1ae7cf64d2540
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-26 18:21:37 -0400

    Update half
    Add logic to determine ship type based on random value.

--- SpawnShip.asm
+++ SpawnShip.asm
@@ -1,1 +1,18 @@
 INCLUDE rocketGame.inc
+
+.code
+SpawnShip PROC
+    mov  eax, 3
+    call RandomRange
+    mov  shipType, eax
+
+    mov  eax, shipType
+    cmp  eax, 0
+    je   @small
+    cmp  eax, 1
+    je   @med
+    mov  eax, 3
+    jmp  @sizeDone
+@med:
+    mov  eax, 2
+    jmp  @sizeDone
