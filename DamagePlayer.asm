4e5ed90ec6f7c931fa4d81c2140e7dbc54cff375
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-26 21:10:05 -0400

    Update DamagePlayer.asm

--- DamagePlayer.asm
+++ DamagePlayer.asm
@@ -1,2 +1,22 @@
 INCLUDE rocketGame.inc
 
+DamagePlayer PROC USES eax ebx ecx edx
+
+    mov  eax, nextDamageIdx
+    cmp  eax, 4
+    jge  @done
+
+    mov  ecx, eax
+    shl  ecx, 2
+    mov  ebx, damageOrder[ecx]
+    not  ebx
+    and  limbMask, ebx
+
+    inc  eax
+    mov  nextDamageIdx, eax
+
+@done:
+    ret
+DamagePlayer ENDP
+
+END
