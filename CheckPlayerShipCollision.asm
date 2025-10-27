7526427a6674f41d7ffb9d2996468a98dffc04fb
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-26 21:27:09 -0400

    Update collision detection logic in assembly

--- CheckPlayerShipCollision.asm
+++ CheckPlayerShipCollision.asm
@@ -18,3 +18,21 @@ CheckPlayerShipCollision PROC USES eax ebx ecx edx esi edi ebp
 @big:
     mov  edx, 10
     mov  ebx, 3
+
+@sz_ok:
+
+    mov  eax, shipX                   ; sx0
+    mov  ecx, eax                     ; temp = sx0
+    add  ecx, edx                     ; temp = sx0 + W
+    dec  ecx                          ; sx1
+
+    mov  esi, shipY                   ; sy0
+    mov  edx, esi                     ; temp = sy0
+    add  edx, ebx                     ; temp = sy0 + H
+    dec  edx                          ; sy1   (EDX holds sy1)
+
+    mov  ebx, stickX                  ; px center
+    mov  edi, ebx                     ; px1 base
+    sub  ebx, 1                       ; px0 = x-1
+    add  edi, 1                       ; px1 = x+1
+
