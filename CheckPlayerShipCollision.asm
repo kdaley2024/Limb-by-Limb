fbce89b7223c166940691dfa343a95a7b0ed737e
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-26 21:24:47 -0400

    Add collision check logic for player ship

--- CheckPlayerShipCollision.asm
+++ CheckPlayerShipCollision.asm
@@ -1,1 +1,20 @@
 INCLUDE rocketGame.inc
+
+CheckPlayerShipCollision PROC USES eax ebx ecx edx esi edi ebp
+
+    cmp  playerIFrames, 0
+    jg   @skip
+
+    mov  edi, shipType
+    mov  edx, 6
+    mov  ebx, 2
+    cmp  edi, 0
+    je   @sz_ok
+    cmp  edi, 1
+    jne  @big
+    mov  edx, 8
+    mov  ebx, 2
+    jmp  @sz_ok
+@big:
+    mov  edx, 10
+    mov  ebx, 3
