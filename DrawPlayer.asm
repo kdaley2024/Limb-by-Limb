71b188ac29b1933ee5be77bcb85c6068a558edfa
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-29 10:37:57 -0400

    Create DrawPlayer.asm with stickman drawing setup
    Added basic structure for DrawPlayer procedure.

---
+++ DrawPlayer.asm
@@ -0,0 +1,15 @@
+INCLUDE LimbbyLimb.inc
+
+;color constants that may be used for the implementation of the stickman
+BLACK   EQU 0
+WHITE   EQU 15
+LIGHTRED EQU 12
+LIGHTCYAN EQU 11
+
+.code
+
+;this procedure will draw the stickman in parts
+DrawPlayer PROC USES eax ebx edx
+    ret
+DrawPlayer ENDP
+END
