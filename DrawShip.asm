907c2ddc1c1831e75e6265199a5814236c8d273d
kdaley2024 <kdaley2024@my.fit.edu>
2025-10-30 00:27:03 -0400

    Modify DrawShip procedure for size handling
    Adjust drawing parameters for different ship types.

--- DrawShip.asm
+++ DrawShip.asm
@@ -8,6 +8,19 @@ LIGHTCYAN EQU 11
 .code
 ;this is the procedure for drawing the spaceships in different sizes.
 DrawShip PROC USES eax ebx ecx edx edi
+
+    mov  edi, shipType
+    mov  edx, 6
+    mov  ebx, 2
+    cmp  edi, 0
+    cmp  edi, 1
+    jne  @big
+    mov  edx, 8
+    mov  ebx, 2
+@big:
+    mov  edx, 10
+    mov  ebx, 3
+
     ret
 DrawShip ENDP
 END
