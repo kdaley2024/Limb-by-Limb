eb18f11f19121b097e8aa5825fd7d854ac06ff82
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-01 23:57:47 -0400

    Update UpdateDifficulty.asm

--- UpdateDifficulty.asm
+++ UpdateDifficulty.asm
@@ -1,1 +1,9 @@
 INCLUDE LimbbyLimb.inc
+
+;this procedure increases the difficulty of the game making the spaceships appear and fall on the screen faster
+.code
+UpdateDifficulty PROC USES eax
+    ret
+UpdateDifficulty ENDP
+
+END
