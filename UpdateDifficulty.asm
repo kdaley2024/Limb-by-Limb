92051fa5a192666e20647c8fccd3a2e786b03c68
kdaley2024 <kdaley2024@my.fit.edu>
2025-11-01 23:58:39 -0400

    Implement time check for difficulty increase
    Add logic to check elapsed time before updating difficulty.

--- UpdateDifficulty.asm
+++ UpdateDifficulty.asm
@@ -3,6 +3,13 @@ INCLUDE LimbbyLimb.inc
 ;this procedure increases the difficulty of the game making the spaceships appear and fall on the screen faster
 .code
 UpdateDifficulty PROC USES eax
+
+    call GetTickCount@0
+    mov  edx, lastBumpMs
+    cmp  eax, edx
+    jb   @skip
+
+@skip:
     ret
 UpdateDifficulty ENDP
 
